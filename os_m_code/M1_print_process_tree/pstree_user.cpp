f6bb898e62cdf773282eaf1266cd3b9de236b492
cpy <cpyhal3515@gmail.com>
2023-11-25 07:12:34 -0800

    学习了 C++ 的新特性，将 C 与 C++ 混合编程转换成 C++ 形式

--- os_m_code/M1_print_process_tree/pstree_user.cpp
+++ os_m_code/M1_print_process_tree/pstree_user.cpp
@@ -1,12 +1,11 @@
-#include <stdio.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <dirent.h>
-#include <string.h>
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <queue>
 #include <map>
+#include <cassert>
+#include <dirent.h>
+#include <cstdio>
 using namespace std;
 
 #define IN_PARAM_LOC 1
@@ -19,21 +18,22 @@ using namespace std;
 enum PARAM {PIDS, VERSION};
 
 // 定义信息 struct
-struct Inform_struct {
-   char  name[INFROM_BUF_LEN];
-   int   self_id;
-   int   father_id;
+struct Inform_struct 
+{
+  string name;
+  int   self_id;
+  int   father_id;
 } inform_list[INFORM_STRUCT_NUM];
 
 // 建立 pid 号到名称 name 的映射
 map<int, string> pid2name;
 
 // 定义树的节点
-struct Process_node {
+struct Process_node 
+{
   int pid;
   vector<Process_node*> child_node_list;
 };
-
 
 // 函数声明
 void action_for_diff_param (enum PARAM param);
@@ -42,27 +42,27 @@ struct Process_node* construct_pid_tree(void);
 void printf_pid_tree(struct Process_node* pid_tree, string & printf_string, enum PARAM param);
 
 int process_num = 0;
-
 int main(int argc, char *argv[]) 
 {
   enum PARAM input_param;
   // 完成输入参数的解析
-  assert(argv[IN_PARAM_LOC]); // C 标准保证
-  if(strcmp(argv[IN_PARAM_LOC], "-p") == 0 || strcmp(argv[IN_PARAM_LOC], "--show-pids") == 0)
+  assert(argv[IN_PARAM_LOC]); 
+  if(string(argv[IN_PARAM_LOC]) == "-p" || string(argv[IN_PARAM_LOC]) == "--show-pids")
     input_param = PIDS;
-  else if(strcmp(argv[IN_PARAM_LOC], "-V") == 0 || strcmp(argv[IN_PARAM_LOC], "--version") == 0 )
+  else if(string(argv[IN_PARAM_LOC]) == "-V" || string(argv[IN_PARAM_LOC]) == "--version")
     input_param = VERSION;
   else
   {
-    printf("Parameter error: Only -p(--show-pids), -V(--version)\n");
+    cout << "Parameter error: Only -p(--show-pids), -V(--version)" << endl;
     return -1;
   }
-  assert(!argv[argc]); // C 标准保证
+  assert(!argv[argc]); 
 
   action_for_diff_param (input_param);
 
   return 0;
 }
+
 
 /*
  * action_for_diff_param - 根据不同的参数执行不同的操作
@@ -80,7 +80,7 @@ void action_for_diff_param (enum PARAM param)
   }
   else if(param == VERSION)
   {
-    printf("pstree_user-64 version %s\n", PSTREE_USER_VERSION);
+    cout << "pstree_user-64 version " << PSTREE_USER_VERSION << endl;
   }
   return;
 }
@@ -94,7 +94,8 @@ void action_for_diff_param (enum PARAM param)
 void get_proc_list(void)
 {
   DIR* proc_dir = opendir("/proc");
-  if (proc_dir == NULL) {
+  if (proc_dir == NULL) 
+  {
       perror("Error opening /proc directory");
       exit(1);
   }
@@ -109,44 +110,38 @@ void get_proc_list(void)
           int pid = atoi(entry->d_name);
           if (pid > 0) 
           {
-            FILE* ptr;
-            char path_buf[FILE_PATH_STR_LEN];
-            sprintf(path_buf, "/proc/%d/status", pid);
+            // FILE* ptr;
+            string path_buf = "/proc/" + to_string(pid) + "/status";
 
             // 打开文件，读模式
-            ptr = fopen(path_buf, "r");
-
-            if (NULL == ptr) {
-                printf("File can't be opened \n");
+            string line;
+            ifstream pid_file(path_buf);
+            if (pid_file.is_open()) 
+            {
+                while (getline(pid_file, line)) 
+                {
+                    if(line.substr(0, sizeof("Name") - 1) == "Name")
+                    {
+                      inform_list[process_num_cnt].name = line.substr(sizeof("Name") + 1, line.size() - sizeof("Name"));
+                    }
+                    else if(line.substr(0, sizeof("Pid") - 1) == "Pid")
+                    {
+                      inform_list[process_num_cnt].self_id = stoi(line.substr(sizeof("Pid") + 1, line.size() - sizeof("Pid")));
+                      pid2name[stoi(line.substr(sizeof("Pid") + 1, line.size() - sizeof("Pid")))] = inform_list[process_num_cnt].name;
+                    }
+                    else if(line.substr(0, sizeof("PPid") - 1) == "PPid")
+                    {
+                      inform_list[process_num_cnt].father_id = stoi(line.substr(sizeof("PPid") + 1, line.size() - sizeof("PPid")));
+                      break;
+                    }
+                }
+                process_num_cnt++;
+                pid_file.close();
             }
-
-
-            // 循环读入 status 中全部的行
-            char inform_buf[INFROM_BUF_LEN + 10];   
-            char* gets_status;
-            do {
-                gets_status = fgets(inform_buf, INFROM_BUF_LEN, ptr);
-                if(strncmp(inform_buf, "Name", sizeof("Name") - 1) == 0)
-                {
-                  inform_buf[strcspn(inform_buf, "\n")] = '\0';
-                  strcpy(inform_list[process_num_cnt].name, inform_buf + sizeof("Name") + 1); 
-                }
-                else if(strncmp(inform_buf, "Pid", sizeof("Pid") - 1) == 0)
-                {
-                  inform_list[process_num_cnt].self_id = atoi(inform_buf + sizeof("Pid"));
-                  pid2name[atoi(inform_buf + sizeof("Pid"))] = inform_list[process_num_cnt].name;
-                }
-                else if(strncmp(inform_buf, "PPid", sizeof("PPid") - 1) == 0)
-                {
-                  inform_list[process_num_cnt].father_id = atoi(inform_buf + sizeof("PPid"));
-                  break;
-                }
-            } while (gets_status != NULL);
-
-            process_num_cnt++;
-
-            // 关闭文件
-            fclose(ptr);
+            else 
+            {
+                cout << "Unable to open file" << endl;
+            }
           }
       }
   }
